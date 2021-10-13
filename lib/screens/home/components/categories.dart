27958dd8fc9bae191ffb33abe5bea7094aef4749
Barry Wolfson <bawolfso@microsoft.com>
2021-10-13 17:36:31 -0400

    use option 2 for instrumenting with EI sdk

--- lib/screens/home/components/categories.dart
+++ lib/screens/home/components/categories.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../../constants.dart';
 
-// We need satefull widget for our categories
+// We need stateful widget for our categories
 
 class Categories extends StatefulWidget {
   @override
